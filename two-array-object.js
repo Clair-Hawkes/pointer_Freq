946fb547201f99056c1e0ecb226e8160d38b530d
tdhood <taylordannhood@gmail.com>
2022-05-27 15:57:12 -0700

    problem solved!

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,31 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+
+//should take 2 array of various lengths;
+//first array contains keys
+//second array contains values
+//returns obj created from keys and values
+//if not enough values, keys = null
+// if not enough keys, no values
+
+
+/**function takes two arrays(keys, values) and returns an object of the keys and values
+ * if more keys, set extra keys to null
+ * if more values, do not include
+ * returns object
+ */
+
+function twoArrayObject(keys, values) {
+    const keysAndValues = {};
+    for(let i = 0; i < keys.length; i++) {
+        keysAndValues[keys[i]] = values[i];
+    }
+    for(let objKey in keysAndValues) {
+        if(keysAndValues[objKey] === undefined) {
+            keysAndValues[objKey] = null;
+        }
+    }
+    return keysAndValues;
 }
+
