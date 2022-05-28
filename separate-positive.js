56ee756ee79ce792cbdc0d7a59f707f11f55d1d7
tdhood <taylordannhood@gmail.com>
2022-05-27 17:22:38 -0700

    not quite right

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,25 @@
+"use strict";
+//take an array of non-zero numbers
+//separate the +num to the left and -num to the right
+//don't sort
+//return orginal updated array
+//O(n)
+
+/**Function separatePositive() takes an array of non-zero numbers and returns orginal array with +num on left and -num on right
+ * [2, -1, -3, 6] => [2, 6, -3, -1]
+  */
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+
+function separatePositive(nonZeros) {
+    const negatives = [];
+
+    for(let num of nonZeros) {
+        if (num < 0) {
+            negatives.push(nonZeros.splice(num,1))
+        }
+    }
+    for(let num of negatives) {
+        nonZeros.push(num)
+    }
+    return nonZeros
 }
